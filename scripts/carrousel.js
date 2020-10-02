f7579a0b25ed33f29d9c7ad9db8e1aa92a4ae284
RoM <rociocmena@gmail.com>
2020-10-01 22:04:58 -0300

    fullscreen mode improved, but  not ready yet

--- scripts/carrousel.js
+++ scripts/carrousel.js
@@ -2,73 +2,104 @@
 
 //1.a. Busca y rellena los trending GIFs.
 
-const trendingCardsCtn = document.getElementById("trending-cards-ctn");
-const trendingCardTemplate = trendingCardsCtn.children[0].content.firstElementChild;
+const trendingCardsCtn = document.getElementById('trending-cards-ctn');
+const trendingCardTemplate =
+	trendingCardsCtn.children[0].content.firstElementChild;
 displayTrendingGifs(trendingCardsCtn);
 
-
 async function displayTrendingGifs(trendingCardsCtn) {
-    let gifs = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=10`);
-    try {
-        if (gifs.status!=200) throw new Error("No se han podido cargar los GIFs.");
-        
-        gifs = await gifs.json();
-        gifs = gifs.data ///Ahora es un array con gifs en toda regla.
-        gifs.forEach(gif => addGifToDOM(gif,trendingCardsCtn)); //Crea cada card y la agrega al DOM.
-    }
-    catch(error) {
-        console.log(`Trending Gifs: \n${error}`);
-    } 
+	let gifs = await fetch(
+		`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=10`
+	);
+	try {
+		if (gifs.status != 200)
+			throw new Error('No se han podido cargar los GIFs.');
+
+		gifs = await gifs.json();
+		gifs = gifs.data; ///Ahora es un array con gifs en toda regla.
+		gifs.forEach((gif) => addGifToDOM(gif, trendingCardsCtn)); //Crea cada card y la agrega al DOM.
+	} catch (error) {
+		console.log(`Trending Gifs: \n${error}`);
+	}
 }
 
 function addGifToDOM(gif, gifsCtn) {
-    let ctn = trendingCardTemplate.cloneNode(true);
-    //ctn.id = gif.id; /////EEEEEh, NO PUEDO HACER ESOOOOO. Podrían repetirse. Para tener el id del gif a mano cuando lo quiera guardar en favoritos.
-    ctn.children[0].src = gif.images.fixed_height_small.url; //Setea el gif.
-    ctn.children[0].alt = gif.title;
-    let gifData = gif.title.split("GIF by "); //Obtiene el título del gif y el usuario.
-    ctn.children[2].children[0].textContent = gifData[1]; //Setea el usuario.
-    ctn.children[2].children[1].textContent = gifData[0]; //Setea el título.
-    
-    //Funcionalidad de los botones:
-    const btnCtn = ctn.children[1]; //Trae la botonera.
-    let favorites = JSON.parse(localStorage.getItem("favGifs"));
-    if (favorites.includes(gif.id)) btnCtn.children[0].src = "../images/icon-trash-hover.svg";
-    
-    btnCtn.children[0].gifId = gif.id; ///No sé si ponerlo en el botón fav o en la botonera completa.
-    btnCtn.children[0].addEventListener("click",addFavorite);
-    btnCtn.children[2].addEventListener("click",displayFullScreen);
-    
-    btnCtn.children[1].href = gif.images.original.url;
-    //btnCtn.children[1].download = gif.title
+	let ctn = trendingCardTemplate.cloneNode(true);
+	//ctn.id = gif.id; /////EEEEEh, NO PUEDO HACER ESOOOOO. Podrían repetirse. Para tener el id del gif a mano cuando lo quiera guardar en favoritos.
+	ctn.children[0].src = gif.images.fixed_height_small.url; //Setea el gif.
+	ctn.children[0].alt = gif.title;
+	let gifData = gif.title.split('GIF by '); //Obtiene el título del gif y el usuario.
+	ctn.children[2].children[0].textContent = gifData[1]; //Setea el usuario.
+	ctn.children[2].children[1].textContent = gifData[0]; //Setea el título.
 
-    ///console.log(gif)
+	//Funcionalidad de los botones:
+	const btnCtn = ctn.children[1]; //Trae la botonera.
+	let favorites = JSON.parse(localStorage.getItem('favGifs'));
+	if (favorites.includes(gif.id)) {
+		btnCtn.children[0].classList.remove("far");
+		btnCtn.children[0].classList.add("fas");
+	}
+	btnCtn.children[0].gifId = gif.id; ///No sé si ponerlo en el botón fav o en la botonera completa.
+	btnCtn.children[0].addEventListener('click', addFavorite);
 
-    gifsCtn.appendChild(ctn);
+	//Dependiendo del viewport, elige si ponerle el fullscreen al botón o a la imagen completa.
+	//Cabe destacar que una vez que se setea al cargar la página, luego no cambia si cambia el viewport.
+	//let viewPort = window.innerWidth || document.documentElement.clientWidth;
+	btnCtn.children[2].addEventListener('click', displayFullScreen); //Le pone el listener al botón de fullscreen.
+	ctn.children[0].addEventListener('click', displayFullScreen); //Le pone el listener a la imagen.
+
+	btnCtn.children[1].href = gif.images.original.url;
+	//btnCtn.children[1].download = gif.title
+
+	///console.log(gif)
+
+	gifsCtn.appendChild(ctn);
 }
 
-
-function addFavorite() { ///Puede que en toda la funciónno tenga que ser this.gifId sino this.parent.gifId
-    let favorites = JSON.parse(localStorage.getItem("favGifs"));
-    if (!favorites.includes(this.gifId)) { //a. Se agrega el gif a favoritos.
-        this.src = "../images/icon-trash-hover.svg";///Conseguite el ícono posta.
-        favorites.push(this.gifId)
-        localStorage.setItem("favGifs",JSON.stringify(favorites));
-        return
-    }
-    //b. El gif ya estaba en favoritos y se elimina:
-    this.src = "../images/icon-fav-hover.svg";///Conseguite el ícono posta.
-    favorites = favorites.filter(favorite => favorite!=this.gifId)
-    localStorage.setItem("favGifs",JSON.stringify(favorites));
+function addFavorite() {
+	///Puede que en toda la funciónno tenga que ser this.gifId sino this.parent.gifId
+	let favorites = JSON.parse(localStorage.getItem('favGifs'));
+	if (!favorites.includes(this.gifId)) {
+		//a. Se agrega el gif a favoritos.
+		this.classList.remove("far");
+		this.classList.add("fas");
+		favorites.push(this.gifId);
+		localStorage.setItem('favGifs', JSON.stringify(favorites));
+		return;
+	}
+	//b. El gif ya estaba en favoritos y se elimina:
+	this.classList.remove("fas");
+	this.classList.add("far");
+	favorites = favorites.filter((favorite) => favorite != this.gifId);
+	localStorage.setItem('favGifs', JSON.stringify(favorites));
 }
 
 function displayFullScreen() {
-    console.log("Mostrar en fullscreen")
+	let gif = this.parentElement;
+	if (!gif.classList.contains('gif-card')) {
+		gif = gif.parentElement;
+	}
+	if (fullscreen.classList.contains('hidden')) {
+		gif.children[1].lastElementChild.classList.remove("fa-download");
+		gif.children[1].lastElementChild.classList.add("fa-times");
+		fullscreen.classList.remove('hidden');
+		gif.setAttribute('id', 'on-fullscreen-gif');
+		return;
+	}
+	gif.children[1].lastElementChild.classList.remove("fa-times");
+	gif.children[1].lastElementChild.classList.add("fa-download");
+	fullscreen.classList.add('hidden');
+	gif.setAttribute('id', '');
+
 }
- 
+
 //2.a. Funcionalidad del carrusel en desktop.
 
 const carrouselCtn = trendingCardsCtn.parentElement.parentElement;
 ///console.log(carrouselCtn);
-carrouselCtn.firstElementChild.addEventListener("click",()=> console.log("move left"));
-carrouselCtn.lastElementChild.addEventListener("click",()=> console.log("move right"));
+carrouselCtn.firstElementChild.addEventListener('click', () =>
+	console.log('move left')
+);
+carrouselCtn.lastElementChild.addEventListener('click', () =>
+	console.log('move right')
+);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const apiKey = "VZ4N6ebz6BSdgrhUNiKAAU0dNYws5GSn";
-if(!localStorage.getItem("favGifs")) localStorage.setItem("favGifs","[]"); //Inicializa los favoritos.
+if (!localStorage.getItem("favGifs")) localStorage.setItem("favGifs", "[]"); //Inicializa los favoritos.
 
 
 
@@ -7,55 +7,66 @@ if(!localStorage.getItem("favGifs")) localStorage.setItem("favGifs","[]"); //Ini
 const headerMenuSwitch = document.getElementById("burguer-switch");
 const homeLogo = document.getElementById("home-logo");
 const navBar = document.getElementById("header-links");
-const hiddenSections = document.querySelectorAll(".hidden-section");
+const hiddenSections = Array.from(document.querySelectorAll(".hidden-section"));
 //Datos: hiddenSection[0] y [1] trae a search-section y searched-section respectivamente.
 //Y [2] y [3] se corresponden a las secciones de favoritos y myGifos respectivamente.
-
+//Y [4] es la pantalla de fullscreen gif.
+const fullscreen = hiddenSections.pop()
 
 navBar.children[0].addEventListener("click", themeSwitch);
+navBar.children[0].addEventListener("click", themeTransition);
 navBar.children[1].addEventListener("click", openFavorites);
 navBar.children[2].addEventListener("click", openMyGifos);
-homeLogo.addEventListener("click",displayHomescreen);
+homeLogo.addEventListener("click", displayHomescreen);
 
 if (localStorage.getItem("darkMode")) themeSwitch();
 
 function themeSwitch() {
     headerMenuSwitch.checked = false;
-    const root = document.documentElement
+    const root = document.documentElement;
+    const switchBtn = navBar.children[0];
     if (root.classList.contains("dark")) {
-        this.textContent = "Modo Nocturno";
+        switchBtn.textContent = "Modo Nocturno";
         root.classList.remove("dark");
         localStorage.removeItem("darkMode");
         return;
     }
-    this.textContent = "Modo Diurno";
+    switchBtn.textContent = "Modo Diurno";
     root.classList.add("dark");
-    localStorage.setItem("darkMode","on");
+    localStorage.setItem("darkMode", "on");
 }
+function themeTransition() {
+    const root = document.documentElement;
+    root.classList.add("theme-transition");
+    window.setTimeout(() => {
+        root.classList.remove("theme-transition")
+    }, 1000);
+}
+
 
 function openFavorites() {
     const favoritesSection = hiddenSections[2];
     const favoritesGifs = JSON.parse(localStorage.getItem("favGifs"));
-    displayGifsByIdGallery(favoritesGifs,favoritesSection);
+    displayGifsByIdGallery(favoritesGifs, favoritesSection);
 }
 function openMyGifos() {
     const myGifosSection = hiddenSections[3];
     const myGifs = JSON.parse(localStorage.getItem("myGifs"));
-    displayGifsByIdGallery(myGifs,myGifosSection);
+    displayGifsByIdGallery(myGifs, myGifosSection);
 }
 
-function displayGifsByIdGallery(gifsIds,gallerySection) {
+function displayGifsByIdGallery(gifsIds, gallerySection) {
 
     headerMenuSwitch.checked = false;
     hiddenSections.forEach(section => section.classList.add("hidden"));
     gallerySection.classList.remove("hidden");
-     
-    if(gifsIds!==null && gifsIds.length!==0) {//Muestra la galería.
+
+    if (gifsIds !== null && gifsIds.length !== 0) {//Muestra la galería.
         gallerySection.children[2].classList.remove("hidden");
         gallerySection.children[3].classList.add("hidden");
         displaySectionGifs(gifsIds, gallerySection.children[2].firstElementChild);
-        if (gifsIds.length <= 12) {gallerySection.children[2].lastElementChild.classList.add("hidden")}
-        else {gallerySection.children[2].lastElementChild.classList.remove("hidden")};
+        if (gifsIds.length <= 12) { gallerySection.children[2].lastElementChild.classList.add("hidden") }
+        else { gallerySection.children[2].lastElementChild.classList.remove("hidden") };
     } else { //Muestra la sección de "No hay Gifos".
         gallerySection.children[2].classList.add("hidden");
         gallerySection.children[3].classList.remove("hidden");
@@ -67,10 +78,10 @@ function displaySectionGifs(gifsIds, gifsCtn) {//Muestra en gifsCtn los gifs cuy
     gifsCtn.innerHTML = "";
     gifsIds.forEach(async (gifId) => {
         try {
-        let gif = await fetch(`https://api.giphy.com/v1/gifs/${gifId}?api_key=${apiKey}`);
-        gif = await gif.json();
-        gif = gif.data;
-        addGifToDOM(gif,gifsCtn);
+            let gif = await fetch(`https://api.giphy.com/v1/gifs/${gifId}?api_key=${apiKey}`);
+            gif = await gif.json();
+            gif = gif.data;
+            addGifToDOM(gif, gifsCtn);
         } catch (error) {
             console.log(error);
         }
@@ -91,33 +102,33 @@ const searchCtn = document.getElementById("search-ctn"); //Contenedor de la barr
 const suggestionsCtn = searchCtn.children[1]; //Contenedor de todas las sugerencias.
 const suggestionTemplate = suggestionsCtn.firstElementChild.content.firstElementChild; //Template para cada sugerencia.
 const searchBar = searchCtn.children[0]; //Contenedor de la cruz, el input y la lupa.
-const searchInput = searchBar.children[1]; 
-const searchButton = searchBar.children[2]; 
+const searchInput = searchBar.children[1];
+const searchButton = searchBar.children[2];
 const clearSearchButton = searchBar.children[0];
 const searchResultsCtn = hiddenSections[1].children[1].firstElementChild; //Contenedor de los resultados de la búsqueda.
 
 
 //1.1. Predicción de búsqueda (suggestions).
 
-searchInput.addEventListener("keyup",getSuggestions); 
+searchInput.addEventListener("keyup", getSuggestions);
 
 async function getSuggestions(e) { //Busca las sugerencias para la frase en searchInput.
-   
-    if(e.key==="Enter" || e.keyCode===13) { //Si el usuario apretó enter, lanza la búsqueda.
+
+    if (e.key === "Enter" || e.keyCode === 13) { //Si el usuario apretó enter, lanza la búsqueda.
         makeSearch();
         return;
     };
-    
+
     let terms = this.value.split(" ").join("+") //Une los términos para la búsqueda.
     ///console.log(terms)
     let suggestions = await fetch(`https://api.giphy.com/v1/tags/related/${terms}?api_key=${apiKey}`)
     try {
-        if (suggestions.status!=200) throw new Error("No se pudieron cargar sugerencias de búsqueda.");
+        if (suggestions.status != 200) throw new Error("No se pudieron cargar sugerencias de búsqueda.");
         suggestions = await suggestions.json();
-        if (suggestions.data.length==0) throw new Error("No se han encontrado sugerencias para la búsqueda.")
+        if (suggestions.data.length == 0) throw new Error("No se han encontrado sugerencias para la búsqueda.")
         suggestions = suggestions.data; //Las sugerencias están en el array data.
-        if (suggestions.length>5) suggestions.splice(5,suggestions.length); //Me quedo sólo con 5 sugerencias.
-        
+        if (suggestions.length > 5) suggestions.splice(5, suggestions.length); //Me quedo sólo con 5 sugerencias.
+
         suggestionsCtn.innerHTML = ""; //Borra las sugerencias anteriores.
         suggestions.forEach(object => addSuggestionToDOM(object.name, suggestionTemplate));
 
@@ -130,8 +141,8 @@ async function getSuggestions(e) { //Busca las sugerencias para la frase en sear
 function addSuggestionToDOM(suggestion, containerTemplate) {
     let ctn = containerTemplate.cloneNode(true);
     ctn.children[1].textContent = suggestion;
-    ctn.addEventListener("mousedown",()=> { //Cuando se clickea una sugerencia, lanza su búsqueda.
-        searchInput.value = ctn.children[1].textContent; 
+    ctn.addEventListener("mousedown", () => { //Cuando se clickea una sugerencia, lanza su búsqueda.
+        searchInput.value = ctn.children[1].textContent;
         makeSearch();
     });
     suggestionsCtn.appendChild(ctn);
@@ -142,18 +153,18 @@ function addSuggestionToDOM(suggestion, containerTemplate) {
 
 //a. Dar vuelta los botones. >
 searchBar.addEventListener("focusin", enableSearchBar);
-searchBar.addEventListener("focusout",disableSearchBar); 
+searchBar.addEventListener("focusout", disableSearchBar);
 
 //b. Realizar búsqueda al apretar la lupa. >
-searchButton.addEventListener("mousedown",makeSearch) 
+searchButton.addEventListener("mousedown", makeSearch)
 
 //c. Funcionalidad del botón vaciar cuadro de búsqueda.
-clearSearchButton.addEventListener("mousedown",() => searchInput.value="")
+clearSearchButton.addEventListener("mousedown", () => searchInput.value = "")
 
 
 
 //>a. Cambia el orden de los íconos en la search-bar.
-function enableSearchBar() {searchBar.classList.add("active-search")};
+function enableSearchBar() { searchBar.classList.add("active-search") };
 function disableSearchBar() {
     searchBar.classList.remove("active-search");
     suggestionsCtn.innerHTML = "";
@@ -169,17 +180,17 @@ async function makeSearch() {
     results = await results.json();
     try {
         if (results.message) throw new Error(results.message);
-        if (results.data.lenght==0) throw new Error("No se han encontrado coincidencias con la búsqueda.")
-    
+        if (results.data.lenght == 0) throw new Error("No se han encontrado coincidencias con la búsqueda.")
+
         const gifs = results.data;
         //console.log(hiddenSections[1]) //Es el contenedor .searched-section
         const sectionTitle = searchResultsCtn.parentNode.parentNode.firstElementChild;
-        sectionTitle.textContent = searchInput.value; 
+        sectionTitle.textContent = searchInput.value;
 
-        
+
         const resultsSection = hiddenSections[1];
         resultsSection.classList.remove("hidden"); //Muestra la sección de searched-gifs.
-        
+
         if (gifs.length === 0) { //Muestra la sección de "No se encontraron resultados".
             resultsSection.children[1].classList.add("hidden");
             resultsSection.children[2].classList.remove("hidden");
@@ -188,13 +199,13 @@ async function makeSearch() {
         //Muestra la galería de gifs encontrados.
         resultsSection.children[1].classList.remove("hidden");
         resultsSection.children[2].classList.add("hidden");
-        if (gifs.length < 12) {resultsSection.children[1].lastElementChild.classList.add("hidden")} //Esconde el botón ver más.
-        else {resultsSection.children[1].lastElementChild.classList.remove("hidden")};
+        if (gifs.length < 12) { resultsSection.children[1].lastElementChild.classList.add("hidden") } //Esconde el botón ver más.
+        else { resultsSection.children[1].lastElementChild.classList.remove("hidden") };
         searchResultsCtn.innerHTML = "";
-        gifs.forEach(gif => addGifToDOM(gif,searchResultsCtn));
+        gifs.forEach(gif => addGifToDOM(gif, searchResultsCtn));
 
-    } 
-    catch(error) {
+    }
+    catch (error) {
         console.log(`Search failed: \n${error}`);
     }
 }
@@ -210,26 +221,27 @@ setTrendingTopics(document.getElementById("trending-topics-ctn"))
 async function setTrendingTopics(trendingTopicsCtn) {
     let topics = await fetch(`https://api.giphy.com/v1/trending/searches?api_key=${apiKey}`);
     try {
-        if (topics.status!=200) throw new Error("No se pudieron cargar los trending topics");
+        if (topics.status != 200) throw new Error("No se pudieron cargar los trending topics");
 
-        topics = await topics.json(); 
-        const topicsList = topics.data.splice(0,5); //De la promesa, obtiene data, que tiene 20 términos y saca sólo 5.
-        topicsList.forEach((topic,index) => addTrendingTopic(topic, trendingTopicsCtn,index)); //Agrega al DOM todos cada topic.
+        topics = await topics.json();
+        const topicsList = topics.data.splice(0, 5); //De la promesa, obtiene data, que tiene 20 términos y saca sólo 5.
+        topicsList.forEach((topic, index) => addTrendingTopic(topic, trendingTopicsCtn, index)); //Agrega al DOM todos cada topic.
     }
-    catch(error) {
+    catch (error) {
         console.log(`Trending Topics: \n${error}`);
     }
 }
 
-function addTrendingTopic(topic, topicsCtn,index) { //Agrega un topic al DOM y le pone un Listener para lanzar el search cuando se lo clickea.
+function addTrendingTopic(topic, topicsCtn, index) { //Agrega un topic al DOM y le pone un Listener para lanzar el search cuando se lo clickea.
     let ctn = document.createElement("span");
-    ctn.textContent = topic; 
+    ctn.textContent = topic;
 
-    ctn.textContent += (index!==4) ? ", " : "  ";
+    ctn.textContent += (index !== 4) ? ", " : "  ";
 
     ctn.addEventListener("click", () => {
-        searchInput.value = ctn.textContent.slice(0,-2); //Lo busca sin la coma.
-        makeSearch()}
+        searchInput.value = ctn.textContent.slice(0, -2); //Lo busca sin la coma.
+        makeSearch()
+    }
     );
     topicsCtn.appendChild(ctn); //Agrega la palabra al párrafo en el DOM.
 }
